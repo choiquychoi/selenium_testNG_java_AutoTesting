adb4a07f07c6e9ce4902155f08630b7c0cab56e5
congquy <112794060+choiquychoi@users.noreply.github.com>
2025-09-10 09:55:46 +0700

    test pro work

---
+++ Selenium/eclipseTips/Topic_04_Access_Modifier.java
@@ -0,0 +1,9 @@
+package eclipseTips;
+
+public class Topic_04_Access_Modifier {
+    public static void main(String[] args) {
+        Topic_03_Getter_Setter topic_03 = new Topic_03_Getter_Setter();
+
+        topic_03.setCarName("ToyoTa Car");
+    }
+}
